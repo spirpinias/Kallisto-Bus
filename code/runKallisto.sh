307276139069f0572d97ff3d2725574fd6a8ba1f
Stephen Pirpinias <stephen@codeocean.com>
2023-03-23 19:26:43 +0000

    Added runKallisto.sh; edited Dockerfile

---
+++ code/runKallisto.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+reference_file=$(find -L ../data -name "*.fasta" -o -name "*.fa")
+forward_reads=$(find -L ../data -name "*_R1.fastq.gz" -o -name "*_R1.fq.gz")
+reverse_reads=$(find -L ../data -name "*_R2.fastq.gz" -o -name "*_R2.fq.gz")
+
+# Construction of the Index
+kallisto index -i ../scratch/transcripts.idx $reference_file
+
+# Quantification
+kallisto quant -i ../scratch/transcripts.idx -o output -b 100 $forward_reads $reverse_reads
+
