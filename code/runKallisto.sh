9cf5070af305929f047f02ddd233da48f028b669
Stephen Pirpinias <stephen@codeocean.com>
2023-03-24 15:06:52 +0000

    Edited runKallisto.sh

--- code/runKallisto.sh
+++ code/runKallisto.sh
@@ -7,19 +7,21 @@ reverse_reads=$(find -L ../data -name "*_R2.fastq.gz" -o -name "*_R2.fq.gz")
 # Construction of the Index
 kallisto index -i ../scratch/transcripts.idx ${reference_file}
 
-if [ "${type_seq}" == "SingleEnded" ];
-then
+
+kallisto quant -i ../scratch/transcripts.idx -o ../results/testing_one -b 10 --single -l 199.757 -s 8.65 ${forward_reads}
+#if [ "${type_seq}" == "SingleEnded" ];
+#then
     # Quantification
-    kallisto quant -i ../scratch/transcripts.idx -o ../results/testing_one -b 10 --single -l 199.757 -s 8.65 ${forward_reads}
-else
-    # Quantification
-    kallisto quant \
-    -i ../scratch/transcripts.idx \
-    -o ../results/testing_one \
-    -t ${num_thread} \
-    ${num_boot} \
-    ${len_frag} \
-    ${std_frag} \
-    ${forward_reads} \
-    ${reverse_reads}
-fi
+    
+#else
+ #   # Quantification
+  #  kallisto quant \
+  #  -i ../scratch/transcripts.idx \
+   # -o ../results/testing_one \
+    #-t ${num_thread} \
+   # ${num_boot} \
+   # ${len_frag} \
+    #${std_frag} \
+    #${forward_reads} \
+    #${reverse_reads}
+#fi
