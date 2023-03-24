17d70f2ffe213a869380f3b66d08dfd4979bb8bf
Stephen Pirpinias <stephen@codeocean.com>
2023-03-24 15:41:37 +0000

    Edited runKallisto.sh

--- code/runKallisto.sh
+++ code/runKallisto.sh
@@ -6,7 +6,12 @@ reference_file=$(find -L ../data -name "*.fasta" -o -name "*.fa")
 forward_reads=$(find -L ../data -name "*_R1.fastq.gz" -o -name "*_R1.fq.gz")
 reverse_reads=$(find -L ../data -name "*_R2.fastq.gz" -o -name "*_R2.fq.gz")
 
+reference_count=$(echo $reference_files | wc -w) 
+forward_count=$(echo $forward_reads | wc -w)
+reverse_count$(echo $reverse_reads | wc -w)
+
 # Construction of the Index
+
 kallisto index -i ../scratch/transcripts.idx ${reference_file}
 
 if [ "${type_seq}" == "SingleEnded" ];
